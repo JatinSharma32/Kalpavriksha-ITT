847d7b5d70c24c1788a6f3a7a24068b7f116dbea
Jatin Sharma <jatinsharma9038@gmail.com>
2025-10-30 08:58:11 +0530

    FIX: Changed the Variable names.

--- Sonar_Image.c
+++ Sonar_Image.c
@@ -4,70 +4,70 @@
 
 void swap(int *num1, int *num2)
 {
-    // Using rotation by swapping value as I found swapping by pointers/addresses is impossible in a contiguous memory of N*N matrix.
+    // Using rotation by swapping value as I found swapping by pointers/addresses is impossible in a contiguous memory of matrixSize*matrixSize matrix.
     int temp = *num1;
     *num1 = *num2;
     *num2 = temp;
 }
-void fillMatrix(int N, int matrixPtr[N][N])
+void fillMatrix(int matrixSize, int matrixPtr[matrixSize][matrixSize])
 {
-    for (int row = 0; row < N; row++)
+    for (int row = 0; row < matrixSize; row++)
     {
-        for (int col = 0; col < N; col++)
+        for (int col = 0; col < matrixSize; col++)
         {
-            *(*(matrixPtr) + (N * row + col)) = rand() % 256;
+            *(*(matrixPtr) + (matrixSize * row + col)) = rand() % 256;
         }
     }
 }
-int printMatrix(int N, int matrixPtr[N][N])
+int printMatrix(int matrixSize, int matrixPtr[matrixSize][matrixSize])
 {
-    for (int row = 0; row < N; row++)
+    for (int row = 0; row < matrixSize; row++)
     {
-        for (int col = 0; col < N; col++)
-            printf("%d\t", *(*(matrixPtr) + (N * row + col)));
+        for (int col = 0; col < matrixSize; col++)
+            printf("%d\t", *(*(matrixPtr) + (matrixSize * row + col)));
         printf("\n");
     }
     printf("\n");
 }
 
-void rotateMatrix(int N, int matrixPtr[N][N])
+void rotateMatrix(int matrixSize, int matrixPtr[matrixSize][matrixSize])
 {
 
-    for (int row = 0; row < N; row++)
+    for (int row = 0; row < matrixSize; row++)
     {
-        for (int col = row + 1; col < N; col++)
+        for (int col = row + 1; col < matrixSize; col++)
         {
-            swap(*(matrixPtr) + (N * row + col), *(matrixPtr) + (N * col + row));
+            swap(*(matrixPtr) + (matrixSize * row + col), *(matrixPtr) + (matrixSize * col + row));
         }
     }
-    for (int row = 0; row < N; row++)
+    for (int row = 0; row < matrixSize; row++)
     {
-        for (int col = 0; col < N / 2; col++)
+        for (int col = 0; col < matrixSize / 2; col++)
         {
-            swap((*(matrixPtr) + (N * row + col)), (*(matrixPtr) + (N * row + (N - 1 - col))));
+            swap((*(matrixPtr) + (matrixSize * row + col)), (*(matrixPtr) + (matrixSize * row + (matrixSize - 1 - col))));
         }
     }
 }
 
-void matrixSmoothing(int N, int matrixPtr[N][N])
+void matrixSmoothing(int matrixSize, int matrixPtr[matrixSize][matrixSize])
 {
-    if (N == 1)
+    if (matrixSize == 1)
         return;
-    int tempArray[N];
+    int tempArray[matrixSize];
     int previous;
-    for (int currRow = 0; currRow < N; currRow++)
+    for (int currRow = 0; currRow < matrixSize; currRow++)
     {
         previous = -1;
-        for (int currCol = 0; currCol < N; currCol++)
+        for (int currCol = 0; currCol < matrixSize; currCol++)
         {
-            int neighborSum = *(*(matrixPtr) + (N * currRow + currCol)), noOfNeighbor = 1;
+            int neighborSum = *(*(matrixPtr) + (matrixSize * currRow + currCol)), noOfNeighbor = 1;
 
             // First take the sum of upper row's 3 elements
             for (int rowIterator = -1; rowIterator <= 1; rowIterator++)
             {
                 if ((currRow - 1) < 0)
                     continue;
-                if ((N * (currRow - 1) + (currCol + rowIterator)) < (N * (currRow - 1)) || (N * (currRow - 1) + (currCol + rowIterator)) > (N * (currRow - 1) + (N - 1)))
+                if ((matrixSize * (currRow - 1) + (currCol + rowIterator)) < (matrixSize * (currRow - 1)) || (matrixSize * (currRow - 1) + (currCol + rowIterator)) > (matrixSize * (currRow - 1) + (matrixSize - 1)))
                     continue;
                 neighborSum += *(tempArray + (currCol + rowIterator));
                 noOfNeighbor++;
@@ -76,23 +76,23 @@ void matrixSmoothing(int N, int matrixPtr[N][N])
             // Then take the sum of down row's 3 elements
             for (int rowIterator = -1; rowIterator <= 1; rowIterator++)
             {
-                if ((currRow + 1) >= N)
+                if ((currRow + 1) >= matrixSize)
                     continue;
-                if ((N * (currRow + 1) + (currCol + rowIterator)) < (N * (currRow + 1)) || (N * (currRow + 1) + (currCol + rowIterator)) > (N * (currRow + 1) + (N - 1)))
+                if ((matrixSize * (currRow + 1) + (currCol + rowIterator)) < (matrixSize * (currRow + 1)) || (matrixSize * (currRow + 1) + (currCol + rowIterator)) > (matrixSize * (currRow + 1) + (matrixSize - 1)))
                     continue;
-                neighborSum += *(*(matrixPtr) + (N * (currRow + 1) + (currCol + rowIterator)));
+                neighborSum += *(*(matrixPtr) + (matrixSize * (currRow + 1) + (currCol + rowIterator)));
                 noOfNeighbor++;
             }
             // Then take current elements left element
-            if ((N * currRow + (currCol - 1)) >= (N * currRow))
+            if ((matrixSize * currRow + (currCol - 1)) >= (matrixSize * currRow))
             {
                 neighborSum += previous;
                 noOfNeighbor++;
             }
             // Then take current elements right element
-            if ((N * currRow + (currCol + 1)) <= (N * currRow + (N - 1)))
+            if ((matrixSize * currRow + (currCol + 1)) <= (matrixSize * currRow + (matrixSize - 1)))
             {
-                neighborSum += *(*(matrixPtr) + (N * currRow + (currCol + 1)));
+                neighborSum += *(*(matrixPtr) + (matrixSize * currRow + (currCol + 1)));
                 noOfNeighbor++;
             }
             int averageValue = neighborSum / noOfNeighbor;
@@ -102,37 +102,37 @@ void matrixSmoothing(int N, int matrixPtr[N][N])
                 *(tempArray + (currCol - 1)) = previous;
             }
             // Update previous with current value
-            previous = *(*(matrixPtr) + (N * currRow + currCol));
+            previous = *(*(matrixPtr) + (matrixSize * currRow + currCol));
 
             // Update current value with average
-            *(*(matrixPtr) + (N * currRow + currCol)) = averageValue;
+            *(*(matrixPtr) + (matrixSize * currRow + currCol)) = averageValue;
         }
         // Update last value of upepr row tempArray with previous
-        *(tempArray + (N - 1)) = previous;
+        *(tempArray + (matrixSize - 1)) = previous;
     }
 }
 
 int main(int argc, char **argv)
 {
     srand(time(NULL));
-    int N;
+    int matrixSize;
     printf("Enter matrix size (2-10): ");
-    if (scanf("%d", &N) != 1 || N < 2 || N > 10)
+    if (scanf("%d", &matrixSize) != 1 || matrixSize < 2 || matrixSize > 10)
     {
         printf("Invalid Input, matrix should be in range [2,10].");
         exit(1);
     }
-    int matrix[N][N];
+    int matrix[matrixSize][matrixSize];
 
-    fillMatrix(N, matrix);
+    fillMatrix(matrixSize, matrix);
     printf("Original Randomly Generated Matrix:\n");
-    printMatrix(N, matrix);
+    printMatrix(matrixSize, matrix);
 
-    rotateMatrix(N, matrix);
+    rotateMatrix(matrixSize, matrix);
     printf("Matrix after 90 Degree Clockwise Rotation:\n");
-    printMatrix(N, matrix);
+    printMatrix(matrixSize, matrix);
 
-    matrixSmoothing(N, matrix);
+    matrixSmoothing(matrixSize, matrix);
     printf("Matrix after Applying 3x3 Smoothing Filter:\n");
-    printMatrix(N, matrix);
+    printMatrix(matrixSize, matrix);
 }
